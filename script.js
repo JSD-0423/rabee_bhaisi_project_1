59efbc60a60a60a092d322d61e7e74e25e9520b5
Rabee <ralbhaisi96@gmail.com>
2023-05-09 22:43:48 +0300

    Project First Submission

---
+++ script.js
@@ -0,0 +1,186 @@
+let courses = [
+  {
+    image: "./assets/images/html.png",
+    description: "Web Development Languages",
+    title: "HTML",
+    author: "Sarah Smith",
+    rating: 80,
+  },
+  {
+    image: "./assets/images/css.webp",
+    description: "Web Development Languages",
+    title: "Angular",
+    author: "David Lee",
+    rating: 70,
+  },
+  {
+    image: "./assets/images/angular.png",
+    description: "Web Development Languages",
+    title: "JavaScript",
+    author: "Emily Chen",
+    rating: 80,
+  },
+  {
+    image: "./assets/images/jquery.png",
+    description: "Frontend Frameworks and Libraries",
+    title: "jQuery",
+    author: "John Johnson",
+    rating: 80,
+  },
+  {
+    image: "./assets/images/angular.png",
+    description: "Frontend Frameworks and Libraries",
+    title: "Angular",
+    author: "Jessica Davis",
+    rating: 75,
+  },
+  {
+    image: "./assets/images/react.webp",
+    description: "Frontend Frameworks and Libraries",
+    title: "React",
+    author: "Daniel Brown",
+    rating: 80,
+  },
+  {
+    image: "./assets/images/vuejs.jpeg",
+    description: "Frontend Frameworks and Libraries",
+    title: "Vue.js",
+    author: "Ava Jones",
+    rating: 75,
+  },
+  {
+    image: "./assets/images/nodejs.webp",
+    description: "Backend Frameworks and Libraries",
+    title: "Node.js",
+    author: "Micheal Kim",
+    rating: 80,
+  },
+  {
+    image: "./assets/images/expressjs.webp",
+    description: "Backend Frameworks and Libraries",
+    title: "Express.js",
+    author: "Sophia Rodriguez",
+    rating: 80,
+  },
+  {
+    image: "./assets/images/ruby.jpeg",
+    description: "Backend Frameworks and Libraries",
+    title: "Ruby and Rails",
+    author: "William Lee",
+    rating: 60,
+  },
+  {
+    image: "./assets/images/angular.png",
+    description: "Frontend Frameworks and Libraries",
+    title: "Angular",
+    author: "Jessica Davis",
+    rating: 80,
+  },
+  {
+    image: "./assets/images/angular.png",
+    description: "Frontend Frameworks and Libraries",
+    title: "Angular",
+    author: "Jessica Davis",
+    rating: 80,
+  },
+  {
+    image: "./assets/images/angular.png",
+    description: "Frontend Frameworks and Libraries",
+    title: "Angular",
+    author: "Jessica Davis",
+    rating: 80,
+  },
+];
+
+const list = document.getElementById("list");
+const btnDarkMode = document.getElementById("btn_dark_mode");
+
+btnDarkMode.addEventListener("click", darkModeToggle);
+
+let darkMode = 0;
+
+const cardFactory = (course) => {
+  // create card element
+  const card = document.createElement("div");
+  card.className = "card";
+
+  // create image element
+  const img = document.createElement("img");
+  img.id = "course_image";
+  img.src = course.image;
+  img.alt = "Course image";
+  card.appendChild(img);
+
+  // create card container element
+  const container = document.createElement("div");
+  container.className = "card_container";
+  card.appendChild(container);
+
+  // create description element
+  const description = document.createElement("p");
+  description.id = "description";
+  description.textContent = course.description;
+  container.appendChild(description);
+
+  // create title element
+  const title = document.createElement("h4");
+  title.id = "title";
+  title.innerHTML = "<b>" + course.title + "</b>";
+  container.appendChild(title);
+
+  // create rating element
+  const rating = document.createElement("div");
+  rating.className = "rating";
+  container.appendChild(rating);
+
+  // create stars element
+  const stars = document.createElement("div");
+  const value = course.rating;
+  const wholeStars = Math.floor((value / 100) * 5);
+  stars.className = `stars stars-${wholeStars}`;
+  rating.appendChild(stars);
+
+  // create author container element
+  const authorContainer = document.createElement("div");
+  authorContainer.id = "author_container";
+  container.appendChild(authorContainer);
+
+  // create author label element
+  const authorLabel = document.createElement("label");
+  authorLabel.htmlFor = "author";
+  authorLabel.textContent = "Author:";
+  authorContainer.appendChild(authorLabel);
+
+  // create author element
+  const author = document.createElement("p");
+  author.id = "author";
+  author.name = "author";
+  author.textContent = course.author;
+  authorContainer.appendChild(author);
+
+  // return the card element
+  return card;
+};
+
+courses.map((course) => {
+  let courseCard = cardFactory(course);
+  list.appendChild(courseCard);
+});
+
+function darkModeToggle() {
+  if (!darkMode) {
+    document.documentElement.style.setProperty("--bg_default", "#1a1a1a");
+    document.documentElement.style.setProperty("--bg_body", "#282828");
+    document.documentElement.style.setProperty("--lines-color", "#000");
+    document.documentElement.style.setProperty("--body-text", "#ededed");
+    darkMode = 1;
+    // window.location.reload();
+  } else {
+    document.documentElement.style.setProperty("--bg_default", "#fff");
+    document.documentElement.style.setProperty("--bg_body", "#f0f9ff");
+    document.documentElement.style.setProperty("--lines-color", "#ddd");
+    document.documentElement.style.setProperty("--body-text", "#333");
+    darkMode = 0;
+    // window.location.reload();
+  }
+}
